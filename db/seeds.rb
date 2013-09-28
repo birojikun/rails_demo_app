0fe48b2784c71940a30e89f94b9664151c60f8b5
sira <birojikun@gmail.com>
2013-09-28 20:35:00 +0900

    Finish demo app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:name => '山田　太郎', :email => 'yamada_tarou@hoge.com')
